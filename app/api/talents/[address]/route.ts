7d00140a212954f7dac64d7914c22876ad4daeb3
Nicolas Wagner <contact@wagner-nicolas.com>
2023-09-10 14:59:54 +0200

    feat: add profile page read only

--- app/api/talents/[address]/route.ts
+++ app/api/talents/[address]/route.ts
@@ -1,47 +1,46 @@
 import { NextRequest } from "next/server";
 import postgres from "postgres";
 
-export async function GET(request: NextRequest) {
-    const searchParamsEntries = request.nextUrl.searchParams.entries();
-    const searchParams = Object.fromEntries(searchParamsEntries);
-  
-    // FIXME: use snake_case instead of camelCase
-    const { address } = searchParams;
-  
-    const sql = postgres(process.env.DATABASE_URL || "", {
-      ssl: {
-        rejectUnauthorized: false, // This allows connecting to a database with a self-signed certificate
-      },
-    });
-  
-    if (!address) {
-      return new Response(
-        JSON.stringify({ message: "Missing address parameter" }),
-        {
-          status: 404,
-        }
-      );
-    }
-  
-    try {
-      const user = await sql`
+export async function GET(
+  request: NextRequest,
+  context: { params: { address: string } }
+) {
+  const { address: walletAddress } = context.params;
+
+  const sql = postgres(process.env.DATABASE_URL || "", {
+    ssl: {
+      rejectUnauthorized: false, // This allows connecting to a database with a self-signed certificate
+    },
+  });
+
+  if (!walletAddress) {
+    return new Response(
+      JSON.stringify({ message: "Missing address parameter" }),
+      {
+        status: 404,
+      }
+    );
+  }
+
+  try {
+    const user = await sql`
         SELECT *
         FROM goodhive.users
-        WHERE wallet_address = ${address}
+        WHERE wallet_address = ${walletAddress}
       `;
-  
-      if (user.length === 0) {
-        return new Response(JSON.stringify({ message: "User not found" }), {
-          status: 404,
-        });
-      }
-  
-      return new Response(JSON.stringify(user[0]));
-    } catch (error) {
-      console.error("Error retrieving data:", error);
-  
-      return new Response(JSON.stringify({ message: "Error retrieving data" }), {
-        status: 500,
+
+    if (user.length === 0) {
+      return new Response(JSON.stringify({ message: "User not found" }), {
+        status: 404,
       });
     }
+
+    return new Response(JSON.stringify(user[0]));
+  } catch (error) {
+    console.error("Error retrieving data:", error);
+
+    return new Response(JSON.stringify({ message: "Error retrieving data" }), {
+      status: 500,
+    });
   }
+}
