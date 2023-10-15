1921187fb350aca021fe9bac06b60f40f5eb289c
Nicolas Wagner <contact@wagner-nicolas.com>
2023-10-15 16:06:33 +0200

    feat: add wording to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
-import { Button } from "./components/button";
 import Image from "next/image";
+
+import { Button } from "./components/button";
 
 export const metadata: Metadata = {
   title: "Home Page | GoodHive",
@@ -10,47 +11,49 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div>
-      <div className=" [background:linear-gradient(180deg,_rgba(214,_194,_188,_0.74),_rgba(0,_0,_0,_0.74))]">
+      <div className="[background:linear-gradient(180deg,_rgba(214,_194,_188,_0.74),_rgba(0,_0,_0,_0.74))]">
         <div className="grid grid-cols-1 sm:grid-cols-2">
-          <div className="relative justify-self-end order-first sm:row-span-2 sm:order-2">
+          <div className="relative order-first justify-self-end sm:row-span-2 sm:order-2">
             <Image
               alt="Bees"
               src="/img/bees.png"
-              width="0"
+              width="0" // width and height are required for next/image but it will be ignored
               height="0"
               sizes="100vw"
               className="w-full h-auto"
             />
           </div>
-          <div className="bg-comb flex flex-col content-around">
-            <div className="p-5 font-bold text-3xl">
-              <h1> Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
+          <div className="flex flex-col content-around bg-comb">
+            <div className="p-5 text-3xl font-bold">
+              <h1>The Decentralized Freelancing Platform</h1>
             </div>
-            <div className="p-5 font-bold text-xl">
-              <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            <div className="p-5 text-xl font-bold">
+              <p>More collaborative, more transparent and fairer than ever.</p>
             </div>
-            <div className="p-5 flex flex-col lg:flex-row justify-around">
-              <Button text="Find a Job" type="primary" size="large"></Button>
-              <Button text="Find a Talent" type="primary" size="large"></Button>
+            <div className="flex flex-col justify-around p-5 lg:flex-row">
+              {/* TODO: Add links to the buttons */}
+              <Button text="Find a Job" type="primary" size="large" />
+              <Button text="Find a Talent" type="primary" size="large" />
             </div>
           </div>
         </div>
       </div>
 
-      <div className="bg-white flex flex-col md:col-span-2 items-center justify-center">
-        <div className=" text-4xl text-black p-10">
-          <b className="">Lorem ipsum dolor sit amet consectetur</b>
+      <div className="flex flex-col items-center justify-center bg-white md:col-span-2">
+        <div className="p-10 text-4xl text-black ">
+          <b className="">Why to Choose GoodHive?</b>
         </div>
-        <div className="text-2xl text-gray p-10">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod
-          tempor
+        <div className="p-10 text-2xl text-gray">
+          GoodHive is platform drive by the community, for the community. We are
+          a group of freelancers, developers, designers, and crypto enthusiasts
+          who are passionate about building a better future for freelancers.
         </div>
       </div>
       <div>
         <Image
           alt="honeycomb-footer"
           src="/img/footer@3x.png"
-          width="0"
+          width="0" // width and height are required for next/image but it will be ignored
           height="0"
           sizes="100vw"
           className="w-full h-auto"
