1d40eb075e49fc4e2303b6df0468eca1d0329378
sabbirrifat <rifat234@gmail.com>
2023-11-17 15:23:43 +0600

    changes are made

--- app/components/home/services/services.tsx
+++ app/components/home/services/services.tsx
@@ -1,21 +1,20 @@
 "use client";
 
-import { FeatureCard } from "@components/featureCard";
+import { FeatureCard } from "@/app/components/feature-card";
 import { TRANSLATION, allServices } from "./services.constants";
 
-import "./services.styles.scss";
 
 export const Services = () => {
   const onCtaClickHandler = (id: string) => {
-    console.log("onCtaClickHandler", id);
+    /* TODO: Implement Service Card click handler */
   };
   return (
-    <div className="container mx-auto services-section">
-      <h1>{TRANSLATION.title}</h1>
-      <p>
+    <div className="container mx-auto w-full pt-12">
+      <h1 className="text-black text-center text-3xl font-bold mb-4">{TRANSLATION.title}</h1>
+      <p className="text-black text-center text-lg font-normal mb-24">
         {TRANSLATION.description}
       </p>
-      <div className="service-cards">
+      <div className="w-full flex flex-wrap justify-between gap-12 mb-24">
         {allServices.map((service) => {
           const { id, title, description, imageSrc, btnText } = service;
           return (
