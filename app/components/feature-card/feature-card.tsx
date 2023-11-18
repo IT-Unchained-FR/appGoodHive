17bfb2c75fa28b7e4c6575799b6fde93e2995ed5
sabbirrifat <rifat234@gmail.com>
2023-11-18 10:41:23 +0600

    fix: changed sass to tailwind and all the issues are resolved

--- app/components/feature-card/feature-card.tsx
+++ app/components/feature-card/feature-card.tsx
@@ -11,19 +11,19 @@ export const FeatureCard: FC<FeatureCardProps> = (props) => {
   };
 
   return (
-    <div className="w-full flex max-w-[470px]">
-      <div className="relative w-[353px] h-[117px] mr-5">
+    <div className="w-full flex max-w-[470px] sm:max-w-[300px]">
+      <div className="relative w-[353px] h-[117px] mr-5 sm:w-[360px] sm:h-[87px]">
         <Image alt="feature card polygon image" src={imageSrc} fill={true} />
       </div>
       <div className="flex flex-col">
-        <h3 className="text-black text-xl font-bold mb-4">
+        <h3 className="text-black text-xl font-bold mb-4 sm:text-base">
           {title}
         </h3>
-        <p className="text-black text-base font-normal mb-3">
+        <p className="text-black text-base font-normal mb-3 sm:text-sm">
           {description}
         </p>
         <button
-          className="w-[120px] h-10 rounded-2xl bg-[#ffc905] border-none text-black text-base font-normal"
+          className="w-[120px] h-10 rounded-2xl bg-[#ffc905] border-none text-black text-base font-normal sm:w-24 sm:h-8 sm:text-xs"
           onClick={onCtaClickHandler}
         >
           {btnText}
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { Metadata } from "next";
 
 import { Hero } from "@components/home/hero";
 import { Services } from "@components/home/services";
-import { HowItWorks } from "@/app/components/home/how-it-works";
-import { ReachUs } from "@/app/components/home/reach-us";
+import { HowItWorks } from "@components/home/how-it-works";
+import { ReachUs } from "@components/home/reach-us";
 
 export const metadata: Metadata = {
   title: "Home Page | GoodHive",
