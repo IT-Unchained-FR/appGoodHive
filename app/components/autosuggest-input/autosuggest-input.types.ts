d1bc1373ab1a0f0329fa5b8cb3741f3360a25cc5
sabbirrifat <rifat234dgh@gmail.com>
2023-11-24 22:12:22 +0600

    feat: all talent profile fields and functionality added

---
+++ app/components/autosuggest-input/autosuggest-input.types.ts
@@ -0,0 +1,5 @@
+export type AutosuggestInputProps = {
+    inputs: string[];
+    selectedInputs: string[];
+    setSelectedInputs: (inputs: string[]) => void;
+}
---
+++ app/talents/my-profile/constant.tsx
@@ -0,0 +1,32 @@
+export const socialLinks = [
+    {
+        name: "linkedin",
+        icon: "/icons/linkedin.svg",
+        placeholder: "Linkedin*",
+        isRequired: true,
+    },
+    {
+        name: "github",
+        icon: "/icons/github.svg",
+        placeholder: "Github",
+        isRequired: false,
+    },
+    {
+        name: 'stackoverflow',
+        icon: '/icons/stackoverflow.svg',
+        placeholder: 'Stackoverflow' ,
+        isRequired: false,
+    },
+    {
+        name: 'telegram',
+        icon: '/icons/telegram.svg',
+        placeholder: 'Telegram',
+        isRequired: true
+    },
+    {
+        name: 'portfolio',
+        icon: '/icons/portfolio.svg',
+        placeholder: 'Portfolio',
+        isRequired: false
+    }
+];
