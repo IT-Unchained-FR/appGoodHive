3669ce24121e608b426542f563f2cf0753190d59
Nicolas Wagner <contact@wagner-nicolas.com>
2023-11-25 11:45:51 +0100

    fix: conflicts

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -40,7 +40,6 @@ export const Card: FC<Props> = ({
   escrowCurrency = "ETH",
   walletAddress,
 }) => {
-  
   const ratePerHour = rate && currency ? `${rate}${currency}/Hour` : null;
   const croppedTitle =
     title.length > 28 ? title.substring(0, 28) + "..." : title;
@@ -55,15 +54,20 @@ export const Card: FC<Props> = ({
       <div className="pl-4 pr-5">
         <div className="flex md:flex-row">
           <div
-            className="relative h-28 w-28 flex items-center justify-center cursor-pointer bg-yellow-300"
+            className="relative flex items-center justify-center bg-yellow-300 cursor-pointer h-28 w-28"
             style={{
               clipPath:
                 "polygon(50% 0, 100% 25%, 100% 75%, 50% 100%, 0 75%, 0 25%)",
             }}
           >
-            <Image className="object-cover" src={profileImage} alt="avatar" fill />
+            <Image
+              className="object-cover"
+              src={profileImage}
+              alt="avatar"
+              fill
+            />
           </div>
-          <div className="pl-8 md:ml-2 pt-2">
+          <div className="pt-2 pl-8 md:ml-2">
             <p className="text-xl font-semibold text-gray-800">
               {croppedTitle}
             </p>
@@ -72,23 +76,21 @@ export const Card: FC<Props> = ({
           </div>
           <div className="flex flex-col items-end pt-2 grow">
             <div className="flex mb-1">
-              <div className="w-10 h-7 mr-3 relative">
+              <div className="relative w-10 mr-3 h-7">
                 <Image alt="balance" src="/icons/money.svg" fill />
               </div>
-              <div className="w-9 h-6 relative">
+              <div className="relative h-6 w-9">
                 <Image src={countryFlag} alt="country" fill />
               </div>
-            )}
-          </div>
-          <div className="w-full flex gap-3 justify-center md:justify-end sm:justify-end ">
-            <Link href={`/talents/${walletAddress}`}>
-              <div className="flex justify-center lg:items-center">
-                <Button text="Know more..." type="secondary" size="medium" />
-              </div>
-            </Link>
-
-            <div className="flex justify-center lg:items-center">
-              <Button text="Apply Now" type="primary" size="medium" />
+            </div>
+            <p className="mb-1 font-light text-gray-500">{city}</p>
+            <div className="flex space-between">
+              <div className="text-base font-bold">{ratePerHour}</div>
+              {!!escrowAmount && (
+                <div>
+                  {escrowAmount?.toString()} {escrowCurrency}
+                </div>
+              )}
             </div>
           </div>
         </div>
@@ -106,7 +108,7 @@ export const Card: FC<Props> = ({
           ))}
         </div>
 
-        <div className="w-full gap-3 flex justify-end sm:flex-col md:flex-col">
+        <div className="flex justify-end w-full gap-3 sm:flex-col md:flex-col">
           <Link href={`/talents/${walletAddress}`}>
             <Button text="Know more..." type="secondary" size="medium" />
           </Link>
