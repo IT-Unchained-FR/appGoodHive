075ec1ab4e11adf90c1657e8ff71c6c6687c7a80
sabbirrifat <rifat234dgh@gmail.com>
2024-01-15 13:38:07 +0600

    all the blockchain functionality done

---
+++ app/companies/create-job/PopupModal/PopupModal.utils.ts
@@ -0,0 +1,28 @@
+export const generateContent = (type: string) => {
+  switch (type) {
+    case "addFunds":
+      return {
+        title: "Add Funds",
+        description: "Enter the amount you want to add to your job",
+        buttonText: "Add Funds",
+      };
+    case "withdraw":
+      return {
+        title: "Withdraw Funds",
+        description: "Enter the amount you want to withdraw from your job contract",
+        buttonText: "Withdraw Funds",
+      };
+    case "transfer":
+      return {
+        title: "Pay The Fees",
+        description: "Enter the amount you want to pay",
+        buttonText: "Pay Fees",
+      };
+    default:
+      return {
+        title: "Add Funds",
+        description: "Enter the amount you want to add to your account",
+        buttonText: "Add Funds",
+      };
+  }
+};
