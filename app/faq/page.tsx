6a35750b0691ed2080e57a8e897a3c045c469df9
sabbirrifat <rifat234dgh@gmail.com>
2024-01-30 08:57:32 +0600

    fix: homepage little issues and text changes are done

---
+++ app/faq/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function FaqPage() {
+  return (
+    <div className="flex w-full h-full justify-center items-center">
+      <h2>Under construction</h2>
+    </div>
+  );
+}
