ed5e25bc60dae451624f229da501567c8d41e286
Sabbir Rifat <rifat234dgh@gmail.com>
2024-02-06 18:01:05 +0600

    fix: homepage small fixes (#153)

--- app/components/home/services/services.tsx
+++ app/components/home/services/services.tsx
@@ -2,6 +2,9 @@
 
 import { useRouter } from "next/navigation";
 import { FeatureCard } from "@/app/components/feature-card";
+import { Button } from "@components/button";
+
+import { GoodhiveQuestLink } from "@constants/common";
 import { TRANSLATION, allServices } from "./services.constants";
 
 export const Services = () => {
@@ -15,11 +18,28 @@ export const Services = () => {
     }
   };
 
+  const onJoinQuestsClickHandler = () => {
+    window.open(GoodhiveQuestLink, "_blank");
+  };
+
   return (
     <div className="container mx-auto w-full pt-12">
       <h1 className="text-black text-center text-3xl font-bold mb-4 sm:text-2xl">
         {TRANSLATION.title}
       </h1>
+      <div className="flex flex-col items-center mb-12">
+        <p className="text-center mb-4">
+          Unlock exclusive referrals rewards and gain governance power. Enjoy
+          privileged access to Airdrops and Whitelists, and become an esteemed
+          holder of our Scout and/or Pioneer NFTs.
+        </p>
+        <Button
+          text="Join our Quests"
+          type="primary"
+          size="small"
+          onClickHandler={onJoinQuestsClickHandler}
+        />
+      </div>
       <p className="text-black text-center text-lg font-normal mb-24 sm:text-base sm:mb-12">
         {TRANSLATION.description}
       </p>
