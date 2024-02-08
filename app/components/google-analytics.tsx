419521c1ee5d8ed5d521298b39886c9d981ae8ae
sabbirrifat <rifat234dgh@gmail.com>
2024-02-09 01:09:39 +0600

    feat: google analytics added

---
+++ app/components/google-analytics.tsx
@@ -0,0 +1,28 @@
+// components/Analytics.js
+import Script from 'next/script'
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID; // Replace with your Measurement ID
+
+const GoogleAnalytics = () => (
+  <>
+    {/* Global Site Tag (gtag.js) - Google Analytics */}
+    <Script
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+    />
+    <Script
+      id="google-analytics"
+      strategy="afterInteractive"
+    >
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${GA_MEASUREMENT_ID}');
+      `}
+    </Script>
+  </>
+);
+
+export default GoogleAnalytics;
