77f14dcc4311e141d2051f1b82f59f902c192250
sabbirrifat <rifat234dgh@gmail.com>
2024-02-09 01:09:39 +0600

    feat: google analytics added

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { Hero } from "@components/home/hero";
 import { Services } from "@components/home/services";
 import { HowItWorks } from "@components/home/how-it-works";
 import { ReachUs } from "@components/home/reach-us";
+import GoogleAnalytics from "@components/google-analytics";
 
 export const metadata: Metadata = {
   title: "Home Page | GoodHive",
@@ -13,6 +14,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="home-page overflow-hidden">
+      <GoogleAnalytics />
       <Hero />
       <Services />
       <HowItWorks />
