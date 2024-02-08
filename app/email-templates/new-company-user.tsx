10944242c6a3f3a98a6ddccfcd65636cbb2cb72b
sabbirrifat <rifat234dgh@gmail.com>
2024-02-09 03:00:53 +0600

    fix: email signature added for all templates

--- app/email-templates/new-company-user.tsx
+++ app/email-templates/new-company-user.tsx
@@ -10,14 +10,15 @@ export const CompanyRegistrationTemplate: React.FC<
       <p>Hi {name},</p>
       <p>
         A warm welcome to GoodHive! 🚀 Your journey in discovering and
-        collaborating with the brightest minds in IT starts now. We&apos;re thrilled
-        to have {name} join our community that&apos;s reshaping the Future of Work
-        with values like excellence, commitment, service, and openness.
+        collaborating with the brightest minds in IT starts now. We&apos;re
+        thrilled to have {name} join our community that&apos;s reshaping the
+        Future of Work with values like excellence, commitment, service, and
+        openness.
       </p>
       <p>
-        Here&apos;s the first step: To ensure your profile is spot-on and aligns with
-        your hiring needs, let&apos;s have a chat! Please schedule a 45-minute
-        meeting with us at{" "}
+        Here&apos;s the first step: To ensure your profile is spot-on and aligns
+        with your hiring needs, let&apos;s have a chat! Please schedule a
+        45-minute meeting with us at{" "}
         <a href="https://calendly.com/benoit-kulesza/45-minutes-meeting.">
           https://calendly.com/benoit-kulesza/45-minutes-meeting.
         </a>{" "}
@@ -31,12 +32,16 @@ export const CompanyRegistrationTemplate: React.FC<
         connect.
       </p>
       <p>
-        We&apos;re looking forward to our conversation and to supporting {name} in
-        navigating this exciting landscape of IT talents. Let&apos;s create amazing
-        opportunities together!
+        We&apos;re looking forward to our conversation and to supporting {name}{" "}
+        in navigating this exciting landscape of IT talents. Let&apos;s create
+        amazing opportunities together!
       </p>
+      <br />
+      <br />
       <p>Best Regards,</p>
       <p>The GoodHive Core Team 🐝</p>
+      <a href="mailto:contact@goodhive.io">contact@goodhive.io</a>
+      <p>GoodHive, 39 rue de l&apos;Arrivée 95880 Enghien les Bains - France</p>
     </div>
   );
 };
