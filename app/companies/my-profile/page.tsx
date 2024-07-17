c4b4e02abb1cd619a0f713967d3f9f3ee9201cfd
Sabbir Rifat <rifat234dgh@gmail.com>
2024-07-17 06:01:34 +0600

    feat: all the new feature and small fixes

--- app/companies/my-profile/page.tsx
+++ app/companies/my-profile/page.tsx
@@ -321,18 +321,21 @@ export default function MyProfile() {
                   Phone Country Code*
                 </label>
                 <div className="relative">
-                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <span className="text-gray-500 sm:text-sm">+</span>
-                  </div>
                   <input
                     className="form-control block w-full px-10 py-2 text-base font-normal text-gray-600 bg-white bg-clip-padding border border-solid border-[#FFC905] rounded-full hover:shadow-lg transition ease-in-out m-0 focus:text-black focus:bg-white focus:border-[#FF8C05] focus:outline-none pl-6"
                     placeholder="Phone Country Code"
                     type="text"
-                    pattern="[0-9]+"
                     name="phone-country-code"
-                    required
-                    maxLength={5}
-                    defaultValue={profileData.phone_country_code}
+                    value={selectedCountry?.phoneCode || "+1"}
+                    disabled={true}
+                    defaultValue={
+                      countries[
+                        countries.findIndex(
+                          (country) => country.value === profileData?.country
+                        )
+                      ]?.phoneCode
+                    }
+                    readOnly
                   />
                 </div>
               </div>
