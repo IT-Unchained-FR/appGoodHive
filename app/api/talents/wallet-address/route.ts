b24b2cc3b63cd3df85a07f438c87000a47320be7
jubayerjuhan <davidjuhan23@gmail.com>
2024-07-28 00:53:53 +0600

    Fix: Added Wallet Address API Routes and Dev Dependency For Framer Motion and NextUI Theme

---
+++ app/api/talents/wallet-address/route.ts
@@ -0,0 +1,56 @@
+import postgres from "postgres";
+
+const sql = postgres(process.env.DATABASE_URL || "", {
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export async function POST(req: Request) {
+  if (req.method === "POST") {
+    const { email } = await req.json();
+
+    if (!email) {
+      return new Response(JSON.stringify({ message: "Email is required." }), {
+        status: 400,
+      });
+    }
+
+    try {
+      // Fetch the wallet address for the user with the given email
+      const users = await sql`
+        SELECT wallet_address FROM auth_users
+        WHERE email = ${email}
+      `;
+
+      if (users.length === 0) {
+        return new Response(
+          JSON.stringify({ message: "No User Found With The Email." }),
+          {
+            status: 404,
+          }
+        );
+      }
+
+      return new Response(
+        JSON.stringify({ walletAddress: users[0].wallet_address }),
+        {
+          status: 200,
+        }
+      );
+    } catch (error) {
+      return new Response(
+        JSON.stringify({
+          message: "There was an error fetching the wallet address.",
+        }),
+        {
+          status: 500,
+        }
+      );
+    }
+  } else {
+    return new Response(JSON.stringify({ message: "Method Not Allowed" }), {
+      status: 405,
+    });
+  }
+}
