e37dd18a8639ac657fb5adb345286eeeec7da462
jubayerjuhan <davidjuhan23@gmail.com>
2024-10-17 21:02:41 +0600

    fix: mobile responsiveness added and latest publications showing on top

--- app/components/blog/PostCard.tsx
+++ app/components/blog/PostCard.tsx
@@ -12,21 +12,25 @@ interface PostCardProps {
 
 const PostCard: React.FC<PostCardProps> = ({ post }) => {
   return (
-    <div className="bg-white rounded-lg shadow-md overflow-hidden">
-      <Image
-        src={post.mainImage.asset.url}
-        alt={post.title}
-        width={400}
-        height={225}
-        className="w-full h-48 object-cover"
-        style={{ aspectRatio: "400/225", objectFit: "cover" }}
-      />
+    <div
+      className="bg-white rounded-lg overflow-hidden"
+      style={{ boxShadow: "rgba(0, 0, 0, 0.08) 0px 0px 0px 1px" }}
+    >
+      {" "}
+      <Link href={`/blog/${post.slug.current}` as any} prefetch={false}>
+        <Image
+          src={post.mainImage.asset.url}
+          alt={post.title}
+          width={400}
+          height={225}
+          className="w-full h-48 object-cover"
+          style={{ aspectRatio: "400/225", objectFit: "cover" }}
+        />
+      </Link>
       <div className="p-6">
-        <h3 className="text-xl font-bold mb-2">
-          <Link href={`/blog/${post.slug.current}` as any} prefetch={false}>
-            {post.title}
-          </Link>
-        </h3>
+        <Link href={`/blog/${post.slug.current}` as any} prefetch={false}>
+          <h3 className="text-xl font-bold mb-2">{post.title}</h3>
+        </Link>
         <div className="flex items-center mb-4">
           <Avatar className="w-8 h-8 mr-2">
             <AvatarImage src={post.author.image.asset.url} alt="Author" />
