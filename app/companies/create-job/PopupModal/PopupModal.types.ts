ab005409140682dc1fbbeb8b91b0d5fef2baf0b8
jubayerjuhan <davidjuhan23@gmail.com>
2024-10-30 14:38:37 +0600

    fix: added token name while adding the funds

--- app/companies/create-job/PopupModal/PopupModal.types.ts
+++ app/companies/create-job/PopupModal/PopupModal.types.ts
@@ -1,8 +1,9 @@
 export type AddFundsModalProps = {
-    jobId: number;
-    open: boolean;
-    type: string;
-    onClose: VoidFunction;
-    onSubmit: (amount: number, type: string) => void;
-    currencyToken: string;
+  jobId: number;
+  open: boolean;
+  type: string;
+  onClose: VoidFunction;
+  onSubmit: (amount: number, type: string) => void;
+  currencyToken: string;
+  currencyLabel: string;
 };
