f24b82bf23aa8d829aa1a48107ef6b688806da40
jubayerjuhan <davidjuhan23@gmail.com>
2024-11-08 22:26:34 +0600

    feat: signup route edited in api point to the new db

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -15,19 +15,33 @@ export async function POST(req: Request) {
         JSON.stringify({ message: "Email and Password are required" }),
         {
           status: 400,
-        }
+        },
       );
     }
 
     try {
+      // Check if the email already exists
+      const existingUser = await sql`
+        SELECT 1 FROM goodhive.users WHERE email = ${email};
+      `;
+
+      if (existingUser.count > 0) {
+        return new Response(
+          JSON.stringify({ message: "Email already exists" }),
+          {
+            status: 409,
+          },
+        );
+      }
+
       // Hash the password
       const hashedPassword = await bcrypt.hash(password, 10);
 
       // Insert the new user into the database
       await sql`
-        INSERT INTO auth_users (
+        INSERT INTO goodhive.users (
           email,
-          password
+          passwordHash
         ) VALUES (
           ${email},
           ${hashedPassword}
@@ -38,14 +52,15 @@ export async function POST(req: Request) {
         JSON.stringify({ message: "User Created Successfully" }),
         {
           status: 200,
-        }
+        },
       );
     } catch (error) {
+      console.error("Error creating user", error);
       return new Response(
         JSON.stringify({ message: "There was an error creating your account" }),
         {
           status: 500,
-        }
+        },
       );
     }
   } else {
