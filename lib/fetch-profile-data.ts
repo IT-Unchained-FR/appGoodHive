07ac7e6a8f5e49a25521607b1580000c45a9c831
jubayerjuhan <davidjuhan23@gmail.com>
2024-11-18 00:54:44 +0600

    Refactor: Change talents fetching from wallet_address to user_id

--- lib/fetch-profile-data.ts
+++ lib/fetch-profile-data.ts
@@ -6,22 +6,50 @@ const sql = postgres(process.env.DATABASE_URL || "", {
   },
 });
 
-export async function getProfileData(address: string) {
-  if (!address) {
+export async function getProfileData(userId: string) {
+  if (!userId) {
     return {};
   }
 
   try {
-    const user = await sql`
-        SELECT *
-        FROM goodhive.users
-        WHERE wallet_address = ${address}
-      `;
+    const talent = await sql`
+      SELECT * 
+      FROM goodhive.talents 
+      WHERE user_id = ${userId};
+    `;
 
-    if (user.length === 0) {
+    if (talent.length === 0) {
       return {};
     }
-    return user[0];
+
+    // Search For User
+    const user = await sql`
+      SELECT * 
+      FROM goodhive.users 
+      WHERE userid = ${userId};
+    `;
+
+    if (talent.length === 0) {
+      return {};
+    }
+
+    const profileUser = user[0];
+
+    const getStatus = (status: string, isActive: boolean) => {
+      return isActive ? status : "pending";
+    };
+
+    const talentData = {
+      ...talent[0],
+      talent_status: getStatus(profileUser.talent_status, talent[0].talent),
+      mentor_status: getStatus(profileUser.mentor_status, talent[0].mentor),
+      recruiter_status: getStatus(
+        profileUser.recruiter_status,
+        talent[0].recruiter,
+      ),
+    };
+
+    return talentData;
   } catch (error) {
     console.log("Error retrieving data:", error);
     throw new Error("Failed to fetch data from the server");
