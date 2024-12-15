a64ce5bd23c3e0b942ec8c10318d824d05b967e5
jubayerjuhan <davidjuhan23@gmail.com>
2024-12-15 08:39:37 +0600

    feat: showing connect email and password login button conditionally

--- app/user-profile/page.tsx
+++ app/user-profile/page.tsx
@@ -85,7 +85,12 @@ export default function UserProfilePage() {
             <ProfileItem
               icon={Mail}
               label="Email"
-              value={<StatusBadge status={"No Email Connected"} />}
+              value={
+                <StatusBadge
+                  color="green"
+                  status={userProfile.email || "No Email Connected"}
+                />
+              }
             />
             <ProfileItem
               icon={User}
@@ -120,46 +125,48 @@ export default function UserProfilePage() {
               cropped
             />
           </div>
-          <div className="mt-8 flex justify-center">
-            <button
-              onClick={handleConnect}
-              disabled={isConnecting}
-              className="flex bg-[#FFC905] text-[#ffffff] font-bold py-3 px-6 rounded-full transition-all duration-200 shadow-lg shadow-yellow-300/50 hover:shadow-yellow-400/70"
-            >
-              {isConnecting ? (
-                <div className="flex items-center ">
-                  <svg
-                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle
-                      className="opacity-25"
-                      cx="12"
-                      cy="12"
-                      r="10"
-                      stroke="currentColor"
-                      strokeWidth="4"
-                    ></circle>
-                    <path
-                      className="opacity-75"
-                      fill="currentColor"
-                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    ></path>
-                  </svg>
-                  <span className="drop-shadow-[0_1px_1px_rgba(0,0,0,0.2)]">
-                    Connecting...
+          {!userProfile.email && (
+            <div className="mt-8 flex justify-center">
+              <button
+                onClick={handleConnect}
+                disabled={isConnecting}
+                className="flex bg-[#FFC905] text-[#ffffff] font-bold py-3 px-6 rounded-full transition-all duration-200 shadow-lg shadow-yellow-300/50 hover:shadow-yellow-400/70"
+              >
+                {isConnecting ? (
+                  <div className="flex items-center ">
+                    <svg
+                      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
+                    <span className="drop-shadow-[0_1px_1px_rgba(0,0,0,0.2)]">
+                      Connecting...
+                    </span>
+                  </div>
+                ) : (
+                  <span className="flex items-center drop-shadow-[0_1px_1px_rgba(0,0,0,0.2)]">
+                    <Link className="mr-2 h-5 w-5" />
+                    Connect Email and Password Login
                   </span>
-                </div>
-              ) : (
-                <span className="flex items-center drop-shadow-[0_1px_1px_rgba(0,0,0,0.2)]">
-                  <Link className="mr-2 h-5 w-5" />
-                  Connect Email and Password Login
-                </span>
-              )}
-            </button>
-          </div>
+                )}
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
@@ -215,13 +222,25 @@ function ProfileItem({ icon: Icon, label, value, cropped }: ProfileItemProps) {
   );
 }
 
-function StatusBadge({ status }: { status: "pending" | "approved" | string }) {
-  const color =
-    status === "pending" || status === "approved"
-      ? "bg-green-100 text-green-800"
-      : "bg-gray-100 text-yellow-800";
+function StatusBadge({
+  status,
+  color,
+}: {
+  status: "pending" | "approved" | string;
+  color?: "green" | "yellow" | "none";
+}) {
+  let badgeThemeColor;
+  if (status === "approved" || color === "green") {
+    badgeThemeColor = "bg-green-100 text-green-800";
+  } else if (status === "pending" || color === "yellow") {
+    badgeThemeColor = "bg-yellow-100 text-yellow-600";
+  } else {
+    badgeThemeColor = "";
+  }
   return (
-    <span className={`${color} text-sm font-medium px-2 py-1 rounded-full`}>
+    <span
+      className={`${badgeThemeColor} text-sm font-medium px-2 py-1 rounded-full`}
+    >
       {status?.charAt(0).toUpperCase() + status?.slice(1)}
     </span>
   );
