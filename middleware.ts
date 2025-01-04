b6f576d885bdd10f861f8ab3cde9163722a43190
jubayerjuhan <davidjuhan23@gmail.com>
2025-01-05 00:28:24 +0600

    fix: fixed the my-profile authentication

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import Cookies from "js-cookie";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const user_id = req.cookies.get("user_id")?.value;
+
+  // Define the login URL
+  const loginUrl = new URL("/auth/login", req.url);
+
+  // If no token is present, redirect to login
+  if (!user_id) {
+    return NextResponse.redirect(loginUrl);
+  }
+
+  // Allow the request to proceed
+  return NextResponse.next();
+}
+
+// Configure the routes where this middleware applies
+export const config = {
+  matcher: ["/talents/my-profile/:path*", "/talents/my-profile"], // Protect specific routes
+};
