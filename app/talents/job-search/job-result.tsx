38df9f64e375db771214a427a24a147b3031a0f0
jubayerjuhan <davidjuhan23@gmail.com>
2025-01-06 21:37:32 +0600

    fix: stop showing the saved jobs into the job search page

--- app/talents/job-search/job-result.tsx
+++ app/talents/job-search/job-result.tsx
@@ -26,36 +26,43 @@ export interface JobOffer {
 }
 
 export default function JobResult({ jobOffers }: { jobOffers: any[] }) {
+  console.log(
+    jobOffers.map((job) => job.in_saving_stage),
+    "jobOffers...",
+  );
   return (
     <div className="grid grid-cols-3 gap-5 md:gap-4 sm:gap-4 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1">
-      {jobOffers.map((jobOffer, index) => (
-        <Card
-          uniqueId={jobOffer?.user_id}
-          key={index}
-          jobId={jobOffer.id}
-          type="company"
-          title={jobOffer.title}
-          postedBy={jobOffer.companyName} //TODO: connect job_offers table to companies table
-          postedOn={`Posted On ${moment(jobOffer.posted_at).format(
-            "MMMM Do YYYY",
-          )}`}
-          image={jobOffer.image_url || "/img/company_img.png"} //TODO: connect job_offers table to companies table
-          country={jobOffer.country} // TODO: create flag table
-          city={jobOffer.city} //TODO: connect job_offers table to companies table
-          budget={jobOffer.budget}
-          projectType={jobOffer.projectType}
-          currency={jobOffer.currency}
-          description={jobOffer.jobDescription}
-          skills={jobOffer.skills}
-          buttonText="Apply"
-          walletAddress={jobOffer.walletAddress}
-          mentor={jobOffer.mentor}
-          recruiter={jobOffer.recruiter}
-          escrowAmount={jobOffer.escrowAmount}
-          // escrowAmount={jobOffer.escrowAmount} Add escrowAmount to job_offers table
-          // escrowCurrency={jobOffer.escrowCurrency} Add escrowCurrency to job_offers table
-        />
-      ))}
+      {jobOffers.map((jobOffer, index) => {
+        if (jobOffer.in_saving_stage) return <></>;
+        return (
+          <Card
+            uniqueId={jobOffer?.user_id}
+            key={index}
+            jobId={jobOffer.id}
+            type="company"
+            title={jobOffer.title}
+            postedBy={jobOffer.companyName} //TODO: connect job_offers table to companies table
+            postedOn={`Posted On ${moment(jobOffer.posted_at).format(
+              "MMMM Do YYYY",
+            )}`}
+            image={jobOffer.image_url || "/img/company_img.png"} //TODO: connect job_offers table to companies table
+            country={jobOffer.country} // TODO: create flag table
+            city={jobOffer.city} //TODO: connect job_offers table to companies table
+            budget={jobOffer.budget}
+            projectType={jobOffer.projectType}
+            currency={jobOffer.currency}
+            description={jobOffer.jobDescription}
+            skills={jobOffer.skills}
+            buttonText="Apply"
+            walletAddress={jobOffer.walletAddress}
+            mentor={jobOffer.mentor}
+            recruiter={jobOffer.recruiter}
+            escrowAmount={jobOffer.escrowAmount}
+            // escrowAmount={jobOffer.escrowAmount} Add escrowAmount to job_offers table
+            // escrowCurrency={jobOffer.escrowCurrency} Add escrowCurrency to job_offers table
+          />
+        );
+      })}
     </div>
   );
 }
