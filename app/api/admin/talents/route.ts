9465e86b99f6eb07af600c3495ef34e30dbcb8cc
jubayerjuhan <davidjuhan23@gmail.com>
2025-01-10 15:11:50 +0600

    fix: added talents data fetching with lib

--- app/api/admin/talents/route.ts
+++ app/api/admin/talents/route.ts
@@ -1,42 +1,17 @@
+import { getAdminTalents } from "@/lib/fetch-admin-talents";
 import type { NextRequest } from "next/server";
-import postgres from "postgres";
-
-const sql = postgres(process.env.DATABASE_URL || "", {
-  ssl: {
-    rejectUnauthorized: false, // This allows connecting to a database with a self-signed certificate
-  },
-});
 
 export async function GET(req: NextRequest) {
   try {
-    const talents = await sql`
-      SELECT 
-        t.*,
-        COALESCE(u.wallet_address, null) AS wallet_address
-      FROM goodhive.talents t
-      LEFT JOIN goodhive.users u ON t.user_id = u.userid
-    `;
-
-    return new Response(
-      JSON.stringify({
-        message: "Successfully Retrieved All Talents.",
-        talents,
-      }),
-      {
-        status: 200,
-        headers: {
-          "Cache-Control":
-            "no-store, no-cache, must-revalidate, proxy-revalidate",
-          Pragma: "no-cache",
-          Expires: "0",
-          "Surrogate-Control": "no-store",
-        },
-      },
-    );
+    const talents = await getAdminTalents();
+    return new Response(JSON.stringify(talents), { status: 200 });
   } catch (error) {
     console.log(error, "error..");
-    return new Response(JSON.stringify({ message: "Error fetching talents" }), {
-      status: 500,
-    });
+    return new Response(
+      JSON.stringify({ message: "Error fetching talents data" }),
+      {
+        status: 500,
+      },
+    );
   }
 }
