3167682c609fad2855529339fde704c670e55288
jubayerjuhan <davidjuhan23@gmail.com>
2025-01-10 15:24:19 +0600

    fix: added cache control header

--- app/api/admin/talents/route.ts
+++ app/api/admin/talents/route.ts
@@ -4,13 +4,23 @@ import type { NextRequest } from "next/server";
 export async function GET(req: NextRequest) {
   try {
     const talents = await getAdminTalents();
-    return new Response(JSON.stringify(talents), { status: 200 });
+
+    // Set Cache-Control header to disable caching
+    const headers = new Headers({
+      "Content-Type": "application/json",
+      "Cache-Control": "no-store, max-age=0", // Prevent caching
+    });
+
+    return new Response(JSON.stringify(talents), { status: 200, headers });
   } catch (error) {
     console.log(error, "error..");
     return new Response(
       JSON.stringify({ message: "Error fetching talents data" }),
       {
         status: 500,
+        headers: {
+          "Cache-Control": "no-store, max-age=0", // Prevent caching
+        },
       },
     );
   }
