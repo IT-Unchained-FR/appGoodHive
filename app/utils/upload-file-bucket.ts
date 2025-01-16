b8110a86afdbfadacf01188770013eb6f53ad98c
jubayerjuhan <davidjuhan23@gmail.com>
2025-01-17 03:13:39 +0600

    fix: uploading file - sending as form data

--- app/utils/upload-file-bucket.ts
+++ app/utils/upload-file-bucket.ts
@@ -4,34 +4,26 @@ export const uploadFileToBucket = async (file: File) => {
   return new Promise(async (resolve, reject) => {
     if (!file) return resolve(null);
 
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = async () => {
-      const base64File = reader.result;
-      try {
-        const response = await fetch("/api/upload-file", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            file: base64File,
-            fileType: file.type,
-          }),
-        });
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
 
-        if (response.ok) {
-          const { fileUrl } = await response.json();
-          resolve(fileUrl);
-        } else {
-          toast.error("Failed to upload file");
-          console.error(response.statusText);
-          reject(new Error("Failed to upload file"));
-        }
-      } catch (error) {
-        console.error("Error uploading file:", error);
-        reject(error);
+      const response = await fetch("/api/upload-file", {
+        method: "POST",
+        body: formData, // FormData automatically sets the correct Content-Type
+      });
+
+      if (response.ok) {
+        const { fileUrl } = await response.json();
+        resolve(fileUrl);
+      } else {
+        toast.error("Failed to upload file");
+        console.error(response.statusText);
+        reject(new Error("Failed to upload file"));
       }
-    };
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      reject(error);
+    }
   });
 };
