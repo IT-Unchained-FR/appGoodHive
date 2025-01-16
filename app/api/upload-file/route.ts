ce0ddb58709efb7419d4c61b1a8ad29e8ce26bd7
jubayerjuhan <davidjuhan23@gmail.com>
2025-01-17 00:11:35 +0600

    fix

--- app/api/upload-file/route.ts
+++ app/api/upload-file/route.ts
@@ -1,25 +1,32 @@
-import { S3, PutObjectCommand, GetObjectCommand, ObjectCannedACL } from "@aws-sdk/client-s3";
+import { NextRequest, NextResponse } from "next/server";
+
+import {
+  S3,
+  PutObjectCommand,
+  GetObjectCommand,
+  ObjectCannedACL,
+} from "@aws-sdk/client-s3";
 import { generateFileKeyName } from "@/app/utils/generate-file-key-name";
+
+const s3 = new S3({
+  credentials: {
+    accessKeyId: process.env.B2_KEY_ID || "",
+    secretAccessKey: process.env.B2_APP_KEY || "",
+  },
+  endpoint: process.env.B2_ENDPOINT || "",
+  region: process.env.B2_REGION || "",
+});
 
 export async function POST(request: Request) {
   try {
     const { file, fileType } = await request.json();
 
-    const s3 = new S3({
-      credentials: {
-        accessKeyId: process.env.B2_KEY_ID || "",
-        secretAccessKey: process.env.B2_APP_KEY || "",
-      },
-      endpoint: process.env.B2_ENDPOINT || "",
-      region: process.env.B2_REGION || "",
-    });
-
     const bucketName = process.env.B2_BUCKET || "";
     const keyName = generateFileKeyName(fileType);
 
     // Decode the Base64 file back to binary data
-    const base64Data = file.replace(/^data:.*,/, '');
-    const decodedFile = Buffer.from(base64Data, 'base64');
+    const base64Data = file.replace(/^data:.*,/, "");
+    const decodedFile = Buffer.from(base64Data, "base64");
 
     const putObjectParams = {
       Bucket: bucketName,
@@ -46,11 +53,11 @@ export async function POST(request: Request) {
       await s3.send(new GetObjectCommand(getObjectParams));
       const url = `https://${bucketName}.${hostB2}/${keyName}`;
 
-      return new Response(JSON.stringify({ fileUrl: url }));
+      return NextResponse.json({ fileUrl: url });
     } catch (error) {
-      console.error(error);
+      return NextResponse.error();
     }
   } catch (error) {
-    console.error(error);
+    return NextResponse.error();
   }
 }
