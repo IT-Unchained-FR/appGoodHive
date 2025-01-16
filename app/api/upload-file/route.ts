7696301ba3b6ffbf45d6e0c04b717f9d9174b1ef
jubayerjuhan <davidjuhan23@gmail.com>
2025-01-17 03:13:39 +0600

    fix: uploading file - sending as form data

--- app/api/upload-file/route.ts
+++ app/api/upload-file/route.ts
@@ -1,11 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import {
-  S3,
-  PutObjectCommand,
-  GetObjectCommand,
-  ObjectCannedACL,
-} from "@aws-sdk/client-s3";
+import { S3, PutObjectCommand, ObjectCannedACL } from "@aws-sdk/client-s3";
 import { generateFileKeyName } from "@/app/utils/generate-file-key-name";
 
 const s3 = new S3({
@@ -17,47 +12,43 @@ const s3 = new S3({
   region: process.env.B2_REGION || "",
 });
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   try {
-    const { file, fileType } = await request.json();
+    // Extract the file from the FormData payload
+    const formData = await request.formData();
+    const file = formData.get("file") as File;
+
+    if (!file) {
+      return NextResponse.json({ error: "No file provided" }, { status: 400 });
+    }
 
     const bucketName = process.env.B2_BUCKET || "";
+    const fileType = file.type;
     const keyName = generateFileKeyName(fileType);
 
-    // Decode the Base64 file back to binary data
-    const base64Data = file.replace(/^data:.*,/, "");
-    const decodedFile = Buffer.from(base64Data, "base64");
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
 
     const putObjectParams = {
       Bucket: bucketName,
       Key: keyName,
-      Body: decodedFile,
+      Body: buffer,
       ACL: ObjectCannedACL.public_read,
       ContentType: fileType,
     };
 
-    try {
-      await s3.send(new PutObjectCommand(putObjectParams));
-    } catch (error) {
-      console.error(error);
-    }
-
-    const getObjectParams = {
-      Bucket: bucketName,
-      Key: keyName,
-    };
+    // Upload the file to the S3 bucket
+    await s3.send(new PutObjectCommand(putObjectParams));
 
     const hostB2 = process.env.B2_HOST || "";
+    const fileUrl = `https://${bucketName}.${hostB2}/${keyName}`;
 
-    try {
-      await s3.send(new GetObjectCommand(getObjectParams));
-      const url = `https://${bucketName}.${hostB2}/${keyName}`;
-
-      return NextResponse.json({ fileUrl: url });
-    } catch (error) {
-      return NextResponse.error();
-    }
+    return NextResponse.json({ fileUrl });
   } catch (error) {
-    return NextResponse.error();
+    console.error("Error uploading file:", error);
+    return NextResponse.json(
+      { error: "Failed to upload file" },
+      { status: 500 },
+    );
   }
 }
