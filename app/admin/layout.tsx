4ddf8376942e64c2043b072d4f16c98b763d4450
jubayerjuhan <davidjuhan23@gmail.com>
2025-02-05 01:26:38 +0600

    feat: added admins page and added option for creating new admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -65,6 +65,13 @@ const RootLayout = ({ children }: RootLayoutProps) => {
             <Users className="w-5 h-5 mr-3" />
             All Users
           </Link>
+          <Link
+            href="/admin/admins"
+            className="flex items-center px-4 py-3 text-gray-700 hover:bg-gray-50 hover:text-blue-600"
+          >
+            <Users className="w-5 h-5 mr-3" />
+            Manage Admins
+          </Link>
         </nav>
       </div>
 
