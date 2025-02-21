6b538a4f204028615ad7762fc654c5f2086bbea8
jubayerjuhan <davidjuhan23@gmail.com>
2025-02-21 23:31:50 +0600

    feat: added video embed link on homepage

--- app/components/home/how-it-works/how-it-works.tsx
+++ app/components/home/how-it-works/how-it-works.tsx
@@ -11,28 +11,20 @@ export const HowItWorks = () => {
 
   return (
     <div className="container mx-auto w-full pt-12 pb-12 flex flex-col items-center sm:pt-5">
+      <iframe
+        className="w-[640px] h-[360px] lg:w-[720px] lg:h-[480px] md:w-[640px] md:h-[360px] sm:w-[320px] sm:h-[180px] mb-8"
+        src="https://www.youtube.com/embed/JoAFVaxu3q4"
+        title="GoodHive: Revolutionizing Recruitment for Clients and Web3 Talent!"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
       <h1 className="text-black text-center text-3xl font-bold mb-6 sm:text-2xl">
         {TRANSLATION.title}
       </h1>
       <p className="text-black text-center text-lg font-normal mb-16 sm:text-base sm:mb-8">
         {TRANSLATION.description}
       </p>
-      {/* <div className="mb-12">
-        <div className="relative w-[1350px] h-[534px] lg:w-[720px] lg:h-[350px] md:w-[640px] md:h-[360px] sm:w-[350px] sm:h-[180px]">
-          <Image
-            src="/img/goodhive-mechanism.jpg"
-            alt="Goodhive Mechanism"
-            fill={true}
-          />
-        </div>
-        <iframe
-          className="w-[950px] h-[534px] lg:w-[720px] lg:h-[480] md:w-[640px] md:h-[360px] sm:w-[320px] sm:h-[180px]"
-          src={videoLink}
-          title="GoodHive"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div> */}
+
       <Button
         text="Join our Discord"
         type="primary"
