98598c3b07ca361a40cecc0dfdea667cd82d2ad5
jubayerjuhan <davidjuhan23@gmail.com>
2025-06-20 22:37:47 +0600

    Added saving opt in admin panel for job

--- app/talents/job-search/layout.tsx
+++ app/talents/job-search/layout.tsx
@@ -1,11 +1,9 @@
 import { SearchFilters } from "@/app/components/search-filters";
 import React from "react";
 
-export default function JobSearchLayout({
-  children,
-}: React.PropsWithChildren) {
+export default function JobSearchLayout({ children }: React.PropsWithChildren) {
   return (
-    <main className="container mx-auto">
+    <main className="w-full">
       <SearchFilters />
 
       {children}
