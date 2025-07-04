f24646839ebfd0c32c0c0666381b903c5b81da7f
jubayerjuhan <davidjuhan23@gmail.com>
2025-07-04 22:28:18 +0600

    Added Design For If No Coin Available or Balance available on wallet

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   experimental: {
-    appDir: true,
     typedRoutes: true,
-    serverActions: true,
-    missingSuspenseWithCSRBailout: false,
-    reactStrictMode: false,
-    outputFileTracing: false,
   },
   images: {
     domains: [
