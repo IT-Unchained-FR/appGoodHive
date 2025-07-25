2680a2af3fc1a3db669321ab23abd50ba2b5ddb8
jubayerjuhan <davidjuhan23@gmail.com>
2025-07-25 22:13:15 +0600

    Fix: Showing 2 wallet address in user profile

--- app/user-profile/page.tsx
+++ app/user-profile/page.tsx
@@ -5,8 +5,8 @@ import {
   Briefcase,
   CircleUserRound,
   GraduationCap,
-  Link,
   Mail,
+  Shield,
   User,
   Users,
   Wallet,
@@ -23,6 +23,7 @@ export interface UserProfile {
   mentor_status: "pending" | "approved";
   recruiter_status: "pending" | "approved";
   wallet_address?: string;
+  okto_wallet_address?: string;
 }
 
 export default function UserProfilePage() {
@@ -89,7 +90,7 @@ export default function UserProfilePage() {
           </h1>
         </div>
         <div className="p-6">
-          <div className="grid grid-cols-2 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             <ProfileItem
               icon={Mail}
               label="Email"
@@ -129,53 +130,61 @@ export default function UserProfilePage() {
             />
             <ProfileItem
               icon={Wallet}
-              label="Wallet Address"
+              label="External Wallet Address"
               value={userProfile?.wallet_address || "Not Connected"}
               cropped
             />
+            <ProfileItem
+              icon={Shield}
+              label="GoodHive Wallet Address"
+              value={userProfile?.okto_wallet_address || "Not Connected"}
+              cropped
+            />
           </div>
-          {!userProfile.email && (
-            <div className="mt-8 flex justify-center">
-              <button
-                onClick={handleConnect}
-                disabled={isConnecting}
-                className="flex bg-[#FFC905] text-[#ffffff] font-bold py-3 px-6 rounded-full transition-all duration-200 shadow-lg shadow-yellow-300/50 hover:shadow-yellow-400/70"
-              >
-                {isConnecting ? (
-                  <div className="flex items-center ">
-                    <svg
-                      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <circle
-                        className="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        strokeWidth="4"
-                      ></circle>
-                      <path
-                        className="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                      ></path>
-                    </svg>
-                    <span className="drop-shadow-[0_1px_1px_rgba(0,0,0,0.2)]">
-                      Connecting...
+          {/* <>
+            {!userProfile.email && (
+              <div className="mt-8 flex justify-center">
+                <button
+                  onClick={handleConnect}
+                  disabled={isConnecting}
+                  className="flex bg-[#FFC905] text-[#ffffff] font-bold py-3 px-6 rounded-full transition-all duration-200 shadow-lg shadow-yellow-300/50 hover:shadow-yellow-400/70"
+                >
+                  {isConnecting ? (
+                    <div className="flex items-center ">
+                      <svg
+                        className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <circle
+                          className="opacity-25"
+                          cx="12"
+                          cy="12"
+                          r="10"
+                          stroke="currentColor"
+                          strokeWidth="4"
+                        ></circle>
+                        <path
+                          className="opacity-75"
+                          fill="currentColor"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                      </svg>
+                      <span className="drop-shadow-[0_1px_1px_rgba(0,0,0,0.2)]">
+                        Connecting...
+                      </span>
+                    </div>
+                  ) : (
+                    <span className="flex items-center drop-shadow-[0_1px_1px_rgba(0,0,0,0.2)]">
+                      <Link className="mr-2 h-5 w-5" />
+                      Connect Email and Password Login
                     </span>
-                  </div>
-                ) : (
-                  <span className="flex items-center drop-shadow-[0_1px_1px_rgba(0,0,0,0.2)]">
-                    <Link className="mr-2 h-5 w-5" />
-                    Connect Email and Password Login
-                  </span>
-                )}
-              </button>
-            </div>
-          )}
+                  )}
+                </button>
+              </div>
+            )}
+          </> */}
         </div>
       </div>
     </div>
